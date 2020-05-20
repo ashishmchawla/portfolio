0503e83bf25f115a9b2095d1d226a9a91848e059
Ashish Chawla <ashish@reportcard.ae>
2020-05-21 01:30:59 +0530

    Boiler plate for Website

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/main.css">
+
+    <title> Ashish Chawla | Full Stack Web Developer </title>
+  </head>
+  <body>
+  
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-md-3 col-lg-3 d-none d-md-block" id="sidebar">
+               <div class="linkList">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Work Psych</a>
+                    <a href="#">Portfolio</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>  
+            <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9" id="mainContent">
+                <h1> Main Content </h1>
+            </div>
+        </div>
+
+    </div>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
