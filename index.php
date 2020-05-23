c704da0fd7a6c7011c12cd722829a8875ef2f49b
Ashish Chawla <ashish@reportcard.ae>
2020-05-23 10:31:34 +0530

    Sidebar Navbar Finalization

--- index.php
+++ index.php
@@ -11,25 +11,22 @@
     <title> Ashish Chawla | Full Stack Web Developer </title>
   </head>
   <body>
-  
-    <div class="container-fluid">
-
-        <div class="row">
-            <div class="col-md-3 col-lg-3 d-none d-md-block" id="sidebar">
-               <div class="linkList">
-                    <a href="#">Home</a>
-                    <a href="#">About</a>
-                    <a href="#">Work Psych</a>
-                    <a href="#">Portfolio</a>
-                    <a href="#">Contact</a>
-                </div>
-            </div>  
-            <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9" id="mainContent">
-                <h1> Main Content </h1>
+    <?php include('./sidebar.php'); ?>
+    <div class="row">
+        <div class="col-md-2 col-lg-2 d-none d-md-block" id="sidebar">
+            <div class="linkList">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Blog</a>
+                <a href="#">Contact</a>
             </div>
+        </div>  
+        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10" id="mainContent">
+            
         </div>
-
     </div>
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
