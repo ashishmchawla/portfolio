4781fa298f9dde5afced991acdd27f100f7489e1
Ashish Chawla <ashish@reportcard.ae>
2020-05-25 00:32:43 +0530

    Separating files

--- index.php
+++ index.php
@@ -66,48 +66,7 @@
         <br><br>
       </section>
       <section id="about">
-          <h3 class="sectionHeadingDark"> About Me</h3> 
-          <p>
-            Being a <span class="highlight">techie</span> is something that goes with my persona... <br>
-            I <span class="highlight">love to code</span>, explore technology, their trends and latest innovations.<br>
-            There are too many technologies, systems, and rules that operate this world <br>
-            of internet and that's <span class="highlight">  my playground</span>. Being connected <span class="highlight">online 24/7</span>,<br>
-            part time <span class="highlight">blogger</span>, full-time <span class="highlight">programmer</span>, that's my story on daily basis.<br>
-            I am also a <span class="highlight">tea addict</span> ☕️ about 4-5 cups of it keeps me going throughout the day.
-          </p>
-          <br>
-          <p>
-            Each and every developer has their own style of creating amazing content, so do I.<br>
-            <big> Here's how I do it... </big>
-          </p>
-          <br>
-          <div class="row">
-            <div class="col-md-3">
-              <div class="customCard">
-                <img src="images/plan.png" class="img-fluid">
-                <h4> Strategizing </h4> 
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="customCard">
-                <img src="images/prototype.png" class="img-fluid">
-                <h4> Prototyping </h4> 
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="customCard">
-                <img src="images/design.png" class="img-fluid">
-                <h4> Designing </h4> 
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="customCard">
-                <img src="images/code.png" class="img-fluid">
-                <h4> Coding </h4> 
-              </div>
-            </div>
-          </div>
-          <br>
+        <?php include('about.php'); ?>
       </section>
       <section id="portfolio">
         <h3 class="sectionHeadingLight"> Recent Projects </h3>
