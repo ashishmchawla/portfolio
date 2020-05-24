9bb393fda52d908f2a9c6292dd29392d2b400f2a
Ashish Chawla <ashish@reportcard.ae>
2020-05-24 22:36:13 +0530

    About Section Complete, Portfolio Partial

--- index.php
+++ index.php
@@ -91,8 +91,43 @@
               </div>
             </div>
           </div>
-      
+          <br>
       </section>
+      <section id="portfolio">
+        <h3 class="title"> Portfolio </h3>
+        <br>
+        <div class="row">
+
+          <div class="col-md-4">
+            <div class="cardContainer">
+              <img src="images/4farmers.png" alr="4 Farmers" class="cardImage">
+              <div class="overlay">
+                <div class="cardText">
+                  <h4> 4 Farmers </h4>
+                  <h5> An online grocery store delivering fresh fruits, vegetables and essentials even during the COVID-19 Crisis to certain parts of Mumbai.</h5>
+                  <a href="https://4farmers.in"><i class="material-icons">language</i>&nbsp; 4farmers.in</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="cardContainer">
+              <img src="images/royalSwag.png" alr="4 Farmers" class="cardImage">
+              <div class="overlay">
+                <div class="cardText">
+                  <h4> The Royal Swag </h4>
+                  <h5> An online fashion store for specialized t-shirts for guys and girls that carry a swag! </h5>
+                  <a href="https://theroyalswag.com"><i class="material-icons">language</i>&nbsp; theroyalswag.com</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+
+        </div>
+      </section>
+
     </div>  
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
