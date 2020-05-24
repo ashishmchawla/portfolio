878e575216f017b512e89ffe8811533f9b22137d
Ashish Chawla <ashish@reportcard.ae>
2020-05-24 23:23:23 +0530

    About Section Complete, Portfolio Partial

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ $('#sidebar a').click(function (e) {
     $("html,body").animate({ scrollTop: $(goTo).offset().top }, 1000);
     e.preventDefault();
 });
+
+$('a.nav-link').click(function (e) {
+    $('.navbar-toggler').trigger('click');
+    var goTo = $(this).attr('href'); // selects element that was clicked
+    console.log(goTo);
+    $("html,body").animate({ scrollTop: $(goTo).offset().top }, 1000);
+    e.preventDefault();
+});
