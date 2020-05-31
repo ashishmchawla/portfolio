171003db6a567b3f8325417fd261c24eecf45d64
Ashish Chawla <ashish@reportcard.ae>
2020-05-31 13:48:07 +0530

    Finishing Off

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <h1>
     <small>Hello, </small>
     I'm Ashish Chawla 
-    <br><small> I craft websites and applications...</small>
+    <br><small> I craft websites and applications.</small>
 </h1>
 <p id="home_description"> I am a Full Stack Web Developer from Mumbai. I love to create web-based solutions ranging from anything between a landing page to a feature-rich complete application. </p>
 <center>
